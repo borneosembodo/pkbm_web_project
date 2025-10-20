8fa5afd120e4bf0c913a8dd053be87ecb4a65900
doglar <sembodogelar6@gmail.com>
2025-10-20 14:15:43 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple automatic slideshow
+const slides = document.querySelectorAll(".slides");
+let index = 0;
+
+function showNextSlide() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+
+setInterval(showNextSlide, 4000); // Change image every 4 seconds
